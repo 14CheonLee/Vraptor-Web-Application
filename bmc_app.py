374171057f7baa020d4b2c40ddff3575c373564e
KimMyoungSoo <5qndjddl6@gmail.com>
2019-03-17 18:39:19 +0900

    socketio and post message

--- bmc_app.py
+++ bmc_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
+import requests
 
 app = Flask(__name__)
 app.secret_key = "secret"
@@ -11,6 +12,16 @@ def index():
     data = ["Value 1"]
     return render_template("index.html", data=data)
 
+@socketio.on('connect', namespace='/fan')
+def connect(mes):
+    url = 'http://127.0.0.1:7788/get_fan'
+    # headers = {'Content-type': 'text/html; charset=UTF-8'}
+    data = {'fan_id' : fan.dfdf}
+    response = requests.post(url, data=mes['fan_id'])
+    socketio.emit('response', response)
+
+
 
 if __name__ == '__main__':
-    app.run(port=5566, debug=True)
+    # app.run(port=5566, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5566, use_reloader=False)
