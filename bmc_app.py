d5a5367408a0f0d8b670252556511df8966d4121
ByeongGil-Jung <jbkcose@gmail.com>
2019-03-17 16:28:19 +0900

    [19-03-17] :: Init 2 - Init 2

--- bmc_app.py
+++ bmc_app.py
@@ -1,17 +1,16 @@
-import os
 from flask import Flask, render_template, session
 from flask_socketio import SocketIO, emit
-
 
 app = Flask(__name__)
 app.secret_key = "secret"
 socketio = SocketIO(app)
 
+
 @app.route('/')
 def hello_world():
-    return "templete"
-
+    data = ["Value 1"]
+    return render_template("index.html", data=data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5566, debug=True)
